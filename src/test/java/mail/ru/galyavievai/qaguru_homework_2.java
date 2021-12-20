35e6f118e50b0e0533f1069ecfb67848d4bd9829
Weilson1 <galavievai@gmail.com>
2021-12-20 23:07:58 +0300

    add test for demoqa.com

--- src/test/java/mail/ru/galyavievai/qaguru_homework_2.java
+++ src/test/java/mail/ru/galyavievai/qaguru_homework_2.java
@@ -1,5 +1,7 @@
 package mail.ru.galyavievai;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
@@ -9,8 +11,12 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class qaguru_homework_2 {
 
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+    }
     @Test
-    void succesTest () throws InterruptedException {
+    void succesTest () {
         open("https://demoqa.com/automation-practice-form");
 
         $("#firstName").setValue("John");
@@ -18,13 +24,12 @@ public class qaguru_homework_2 {
         $("#userEmail").setValue("jshepard@email.net");
         $(".custom-control-label").click();
         $("#userNumber").setValue("1111111111");
+        $("#currentAddress").setValue("My address is not a house and not the street, my address is the Soviet Union");
+        $(". css-yk16xz-control").selectOptionByValue("NCR");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__year-select").selectOptionByValue("1999");
         $(".react-datepicker__month-select").selectOptionByValue("2");
         $(".react-datepicker__day").selectOptionByValue("009");
-        Thread.sleep(10000);
-        $("#currentAddress").setValue("My address is not a house and not the street, my address is the Soviet Union");
-        $(".subjects-auto-complete__value-container subjects-auto-complete__value-container--is-multi css-1hwfws3").setValue("Maths");
 
 
 
