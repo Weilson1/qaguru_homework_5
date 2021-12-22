ac24d18085f27b612d68fcbad29068306f70db34
Weilson1 <galavievai@gmail.com>
2021-12-22 19:49:32 +0300

    Внёс доработки

--- src/test/java/ru/mail/galyavievai/qaguru_homework_2.java
+++ src/test/java/ru/mail/galyavievai/qaguru_homework_2.java
@@ -1,4 +1,4 @@
-package mail.ru.galyavievai;
+package ru.mail.galyavievai;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.conditions.Visible;
@@ -16,19 +16,20 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.byXpath;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.files.DownloadActions.click;
+import static java.lang.Thread.sleep;
 
 public class qaguru_homework_2 {
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
     }
-    @AfterAll
-    static void afterAll() {
-    Configuration.holdBrowserOpen = true;
-    }
+
     @Test
-    void succesTest () {
-        open("https://demoqa.com/automation-practice-form");
+    void succesTest() throws InterruptedException {
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
         $("#firstName").setValue("John");
         $("#lastName").setValue("Shepard");
         $("#userEmail").setValue("jshepard@email.net");
@@ -37,24 +38,24 @@ public class qaguru_homework_2 {
         $("#dateOfBirthInput").click();
         $(".react-datepicker__year-select").selectOptionByValue("1994");
         $(".react-datepicker__month-select").selectOptionByValue("2");
-        $(".react-datepicker__day--009").click();
+        $(".react-datepicker__day--009:not(.react-datapicker__day--outside-month)").click();
         $("#subjectsInput").setValue("Maths").pressEnter();
         $("#subjectsInput").setValue("Economics").pressEnter();
         $("#hobbiesWrapper").$(byText("Sports")).click();
         $("#uploadPicture").uploadFromClasspath("img/1.png");
         $("#currentAddress").setValue(
-                "My address is not a house and not the street, my address is the Soviet Union");
+                "My address is not a house and not the street, my address is the Soviet Union").scrollTo();
         $("#state").click();
         $("#stateCity-wrapper").$(byText("NCR")).click();
         $("#city").click();
         $("#stateCity-wrapper").$(byText("Delhi")).click();
         $("#submit").click();
-        $(byText("Thanks for submitting the form")).shouldHave(text("Thanks for submitting the form"));
 // Проверка теста
+        $(byText("Thanks for submitting the form")).shouldHave(text("Thanks for submitting the form"));
         $(".table-responsive").shouldHave(
-                 text("John Shepard"), text("jshepard@email.net"), text("Male"), text("1111111111"),
-                 text("09 March,1994"), text("Maths, Economics"), text("Sports"), text("1.png"),
-                 text("My address is not a house and not the street, my address is the Soviet Union\n"),
-                 text("NCR Delhi"));
+                text("John Shepard"), text("jshepard@email.net"), text("Male"), text("1111111111"),
+                text("09 March,1994"), text("Maths, Economics"), text("Sports"), text("1.png"),
+                text("My address is not a house and not the street, my address is the Soviet Union"),
+                text("NCR Delhi"));
     }
 }
